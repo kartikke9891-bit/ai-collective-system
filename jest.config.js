81ba5c30998291efad499bbabe5cf3ff5469af03
kartikke9891-bit <kartikke9891@gmail.com>
2026-06-01 17:12:40 +0530

    🧪 Create Jest configuration for testing

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'packages/**/src/**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/.next/**'
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 75,
+      functions: 75,
+      lines: 75,
+      statements: 75
+    }
+  },
+  testMatch: [
+    '**/__tests__/**/*.(test|spec).(js|jsx|ts|tsx)',
+    '**/*.(test|spec).(js|jsx|ts|tsx)'
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }]
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  moduleDirectories: ['node_modules', '<rootDir>']
+};
